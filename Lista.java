15887ad576db3dddc67671823800f6e35852185d
unknown <enthony10000@gmail.com>
2021-06-23 18:30:09 -0300

    Adicionando

--- Lista.java
+++ Lista.java
@@ -2,12 +2,13 @@ public class Lista extends ExcecaoDivisaoResultadoZero{
     
     int a[] = new int[10];
     int b[] = new int[10];
+    int x;
 
     public int[] getArrayA(){
         return a;
     }
     
-    public int[]getArrayB(){
+    public int[] getArrayB(){
         return b;
     }
 
@@ -19,8 +20,22 @@ public class Lista extends ExcecaoDivisaoResultadoZero{
         this.b = b;
     }
 
+    public void setArrayANaPosicao(int x, int p){   
+        this.x = x;
+        this.a[p] = x;
+    }
+
+    public void setArrayBNaPosicao(int x, int p){
+        this.x = x;
+        this.b[p] = x;
+    }
+
     public void Calcula(int a, int b) throws ExcecaoDivisaoResultadoZero{
-        int i = 2/3;
+        this.a[a] = a;
+        this.b[b] = b;
+
+        int i = a/b;
+
         if(i >= 1 ){
             System.out.println(i);
         }else{
@@ -28,7 +43,12 @@ public class Lista extends ExcecaoDivisaoResultadoZero{
         }    
     }
 
-    public void CalculaDivisaoArray(){
-        
+    public void CalculaDivisaoArray() throws ExcecaoDivisaoResultadoZero{
+        int s = 1;
+        while (s< 10 ){
+            Calcula(s, s);
+            System.out.println(s);
+            s++;
+        }
     }
 }
--- main.java
+++ main.java
@@ -4,11 +4,16 @@ public class Main {
     public static void main(String[] args) throws ExcecaoDivisaoResultadoZero {
         Lista array1 = new Lista();  
         
+        array1.setArrayANaPosicao(4,3);
+        System.out.println(array1.getArrayA()[3]);
+
         try {
-            array1.Calcula(2, 3);
+            array1.Calcula(2, 0);
         } catch(ArithmeticException e){
             System.out.println("Não é possivel dividir por zero!");
         }
+
+        array1.CalculaDivisaoArray();
 
         }
 }
