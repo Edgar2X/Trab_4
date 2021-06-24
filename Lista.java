2cf5c3057f9a702f7e47023375f1ac31e1f8ea1e
unknown <enthony10000@gmail.com>
2021-06-24 01:55:51 -0300

    Fase Final

--- Lista.java
+++ Lista.java
@@ -2,7 +2,6 @@ public class Lista extends ExcecaoDivisaoResultadoZero{
     
     int a[] = new int[10];
     int b[] = new int[10];
-    int x;
 
     public int[] getArrayA(){
         return a;
@@ -21,21 +20,16 @@ public class Lista extends ExcecaoDivisaoResultadoZero{
     }
 
     public void setArrayANaPosicao(int x, int p){   
-        this.x = x;
         this.a[p] = x;
     }
 
     public void setArrayBNaPosicao(int x, int p){
-        this.x = x;
         this.b[p] = x;
     }
 
     public void Calcula(int a, int b) throws ExcecaoDivisaoResultadoZero{
-        this.a[a] = a;
-        this.b[b] = b;
-
+        
         int i = a/b;
-
         if(i >= 1 ){
             System.out.println(i);
         }else{
@@ -44,10 +38,11 @@ public class Lista extends ExcecaoDivisaoResultadoZero{
     }
 
     public void CalculaDivisaoArray() throws ExcecaoDivisaoResultadoZero{
-        int s = 1;
-        while (s< 10 ){
-            Calcula(s, s);
-            System.out.println(s);
+        int s = 0;
+        while (s <= 9 ){
+            int a = this.a[s];
+            int b = this.b[s];
+            Calcula(a, b);
             s++;
         }
     }
--- main.java
+++ main.java
@@ -4,16 +4,38 @@ public class Main {
     public static void main(String[] args) throws ExcecaoDivisaoResultadoZero {
         Lista array1 = new Lista();  
         
-        array1.setArrayANaPosicao(4,3);
-        System.out.println(array1.getArrayA()[3]);
+        array1.setArrayANaPosicao(19,0);
+        array1.setArrayANaPosicao(10,1);
+        array1.setArrayANaPosicao(11,2);
+        array1.setArrayANaPosicao(13,3);
+        array1.setArrayANaPosicao(15,4);
+        array1.setArrayANaPosicao(21,5);
+        array1.setArrayANaPosicao(14,6);
+        array1.setArrayANaPosicao(17,7);
+        array1.setArrayANaPosicao(16,8);
+        array1.setArrayANaPosicao(18,9);
+
+        array1.setArrayBNaPosicao(1,0);
+        array1.setArrayBNaPosicao(3,1);
+        array1.setArrayBNaPosicao(2,2);
+        array1.setArrayBNaPosicao(7,3);
+        array1.setArrayBNaPosicao(1,4);
+        array1.setArrayBNaPosicao(14,5);
+        array1.setArrayBNaPosicao(6,6);
+        array1.setArrayBNaPosicao(7,7);
+        array1.setArrayBNaPosicao(8,8);
+        array1.setArrayBNaPosicao(11,9);
 
         try {
-            array1.Calcula(2, 0);
+            //array1.Calcula(2, 0);
+            array1.CalculaDivisaoArray();
         } catch(ArithmeticException e){
             System.out.println("Não é possivel dividir por zero!");
+        } finally{
+           
         }
 
-        array1.CalculaDivisaoArray();
+        
 
         }
 }
